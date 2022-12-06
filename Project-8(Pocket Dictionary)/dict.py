9e915b61eab4f83b36000a7cf413a18fb828a8a8
NisarLDev <HarusBadah@proton.me>
2022-12-06 12:58:59 +0100

    Project 8

---
+++ Project-8(Pocket Dictionary)/dict.py
@@ -0,0 +1,8 @@
+#Programme previous requirements : $ pip install pydictionary
+
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+
+print(dictionary.meaning("intelligence"))
+#END OF CODE
+#END OF FILE
